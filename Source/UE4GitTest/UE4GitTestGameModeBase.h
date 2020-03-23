e604cc8f923815e1f8a4ea9aab9e1481bfb49167
Allan Oliveira <allanolivei@gmail.com>
2020-03-23 09:29:07 -0300

    initialize

---
+++ Source/UE4GitTest/UE4GitTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4GitTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4GITTEST_API AUE4GitTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
